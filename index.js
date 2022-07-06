a0b14f347d4a0365d7cf5305f7d5a19bd05c6508
Ahmad Software <99181319+Ahmad-Div@users.noreply.github.com>
2022-07-06 12:55:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,24 +23,30 @@ function changeWebMode() {
 }
 
 
+
+
+
+let emailButton = document.querySelectorAll(".sumbit-email-button");
+let emailHolder = document.querySelectorAll(".email-input");
+let alertBox = document.querySelector(".alert-email-input")
+let alertMessage = document.querySelector(".alert-email-message");
+
 //sign in button onClick event
 
 
 let signIn = document.getElementById("sign-in");
 
 signIn.onclick = function () {
-    alert("To sign in\nPut your eamil in  one of the boxes");
+    displayMessage("block", "Input your email in one of the boxes", 1, 0.5)
+    setTimeout(removeMessage, 1000);
 }
 
 
 
 
 
+
 // input email event
-
-let emailButton = document.querySelectorAll(".sumbit-email-button");
-let emailHolder = document.querySelectorAll(".email-input");
-
 
 for (let i = 0; i < emailButton.length; i++) {
     emailButton[i].addEventListener("click", SubmitButtonEvent);
@@ -50,19 +56,37 @@ function SubmitButtonEvent() {
 
     for (let i = 0; i < emailHolder.length; i++) {
         if (emailHolder[i].value == "" && emailHolder[i + 1].value == "") {
-            alert("Please input your email to submit!");
+            displayMessage("block", "You have not entered any email!", 1, 0.5)
+            setTimeout(removeMessage, 1000);
             break;
         } else {
             if ((emailHolder[i].value.includes("@gmail.com") &&
                 emailHolder[i].value.length >= 10) || (emailHolder[i + 1].value.includes("@gmail.com") &&
                     emailHolder[i + 1].value.length >= 10)) {
-                alert("Email succesfully added!")
+                displayMessage("block", "Email Added!", 1, 0.5)
+                setTimeout(removeMessage, 1000);
                 break;
             } else {
-                alert("Please enter correct email address")
+                displayMessage("block", "Wrong email entered!", 1, 0.5);
+                setTimeout(removeMessage, 1000);
                 break;
             }
         }
     }
 
+}
+
+//function to hide alert box after time
+
+function displayMessage(display, message, mailOpacity, bodyOpacity) {
+    alertBox.style.display = display;
+    alertMessage.innerHTML = message;
+    alertBox.style.opacity = mailOpacity;
+    document.body.style.opacity = bodyOpacity;
+}
+
+
+function removeMessage() {
+    alertBox.style.display = "none";
+    document.body.style.opacity = 1;
 }
